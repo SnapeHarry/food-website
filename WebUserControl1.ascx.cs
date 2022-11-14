2dde2d458594426ad05358e7a616bb0be8dabb90
Siddharam Bijjargi <82494638+SnapeHarry@users.noreply.github.com>
2022-11-14 17:42:11 +0530

    asp.net

---
+++ WebUserControl1.ascx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace middleeast_project.user
+{
+    public partial class WebUserControl1 : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public static string ascx { get; set; }
+    }
+}
---
+++ user.Master.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace middleeast_project.user
+{
+    public partial class user : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Request.Url.AbsoluteUri.ToString().Contains("default.aspx"))
+            {
+                form1.Attributes.Add("class", "sub_page");
+            }
+            else
+            {
+                form1.Attributes.Remove("class");
+                Control webusercontrol1 = (Control)Page.LoadControl("webUserControl1.ascx");
+                Panel1.Controls.Add(webusercontrol1);
+
+            }
+
+        }
+    }
+}
